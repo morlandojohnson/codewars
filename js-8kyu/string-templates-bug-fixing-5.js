acedc65fc809e65f2cccbcf0d2bc20914d50a8be
Morlando Johnson <97807888+Lando241@users.noreply.github.com>
2024-01-23 16:20:02 -0500

    Solution - String Templates-Bug Fixing #5

---
+++ js-8kyu/string-templates-bug-fixing-5.js
@@ -0,0 +1,13 @@
+/*
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+ */
+
+// My Solution:
+
+function buildString(...template) {
+  return `I like ${template.join(", ")}!`;
+}
+
+// Other Solutions:
+
+const buildString = (...template) => `I like ${template.join(", ")}!`;
