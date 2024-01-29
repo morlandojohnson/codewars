d84cc3a930c65921acc7c4567eab23dfb04ef88b
Morlando Johnson <97807888+Lando241@users.noreply.github.com>
2024-01-29 17:31:11 -0500

    Solution - Remove Duplicates from List

---
+++ js-8kyu/remove-duplicates-from-list.js
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION:
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+ */
+
+// My Solution:
+
+function distinct(a) {
+  return [...new Set(a)];
+}
+
+// Other Solutions:
+
+function distinct(a) {
+  return Array.from(new Set(a));
+}
+
+const distinct = (a) => [...new Set(a)];
