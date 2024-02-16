0fbfd8c7456d4c1c156da0529694767128dfbe99
Morlando Johnson <97807888+Lando241@users.noreply.github.com>
2024-02-16 13:17:44 -0500

    Solution - Basic Traning: Adding Item to Array

---
+++ js-8kyu/basic-training-adding-item-to-array.js
@@ -0,0 +1,16 @@
+/*
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+ */
+
+// My Solution:
+
+websites.push("codewars");
+
+// Other Solutions:
+
+websites[0] = "codewars";
