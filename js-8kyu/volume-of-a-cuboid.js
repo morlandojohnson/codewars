e1242fd5e182ee395ec8cfe0fc065be9be69d9b5
Morlando Johnson <97807888+Lando241@users.noreply.github.com>
2024-04-26 11:10:22 -0400

    Solution - Volume of a Cuboid

---
+++ js-8kyu/volume-of-a-cuboid.js
@@ -0,0 +1,12 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+// My Solution:
+// Volume of Cube = length x width x height
+
+function volumeOfCube(length, width, height) {
+  return length * width * height;
+}
+
+// Other Solutions:
+
+const volume = (l, w, h) => l * w * h;
