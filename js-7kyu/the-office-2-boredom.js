b27c9ae669c47f3ba97ea14556ef2c49d4bd78b0
Morlando Johnson <97807888+Lando241@users.noreply.github.com>
2024-08-23 08:47:32 -0400

    Solution - The Office II - Boredom Score

---
+++ js-7kyu/the-office-2-boredom.js
@@ -0,0 +1,59 @@
+/*
+
+You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
+
+Each department has a different boredom assessment score, as follows:
+
+accounts = 1
+finance = 2
+canteen = 10
+regulation = 3
+trading = 6
+change = 6
+IS = 8
+retail = 5
+cleaning = 4
+pissing about = 25
+
+Depending on the cumulative score of the team, return the appropriate sentiment:
+
+<=80: 'kill me now'
+< 100 & > 80: 'i can handle this'
+100 or over: 'party time!!'
+
+d = {'accounts': 1, 'finance': 2, 'canteen': 10, 'regulation': 3, 'trading': 6, 'change': 6, 'IS': 8, 'retail': 5, 'cleaning': 4, 'pissing about': 25}
+
+*/
+
+// My Solution:
+
+/*
+Obtain values from object, count the values
+compare value to scoring
+
+obj = {'accounts': 1, 'finance': 2, 'canteen': 10, 'regulation': 3, 'trading': 6, 'change': 6, 'IS': 8, 'retail': 5, 'cleaning': 4, 'pissing about': 25}
+*/
+
+function boredom(staff) {
+  obj = {
+    accounts: 1,
+    finance: 2,
+    canteen: 10,
+    regulation: 3,
+    trading: 6,
+    change: 6,
+    IS: 8,
+    retail: 5,
+    cleaning: 4,
+    "pissing about": 25,
+  };
+
+  let arr = Object.values(staff).reduce((acc, c) => acc + obj[c], 0);
+  return arr <= 80
+    ? "kill me now"
+    : arr > 100
+    ? "party time!!"
+    : "i can handle this";
+}
+
+// Other Solutions:
